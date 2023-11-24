3ef889edbd6feb01343b6444ba5a729ec04f8c0b
Sourav Barui <74421679+im-sourav@users.noreply.github.com>
2023-11-24 22:13:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function setCanvasSize(parent, cvs, w, h, scale) {
+   parent.style.width = `${w}px`;
+   parent.style.height = `${h}px`;
+   cvs.width = w * scale;
+   cvs.height = h * scale;
+}
+
+const rows = 40;
+const cols = 40;
+const ww = window.innerWidth;
+const hh = window.innerHeight;
+const FPS = 2;
+const scale = 10;
+
+
